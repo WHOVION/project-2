cf982bd7d10067e27f20191b9b4404889b6cf6a9
WHOVION <eszrajs@gmail.com>
2022-12-21 12:34:33 -0800

    login if login matches

--- controllers/users.js
+++ controllers/users.js
@@ -34,5 +34,46 @@ router.post('/', async (req, res) => {
     }
 })
 
+// GET /users/login -- create a login from that POSTs to /users/login
+router.get('/login', (req, res) => {
+    res.render('users/login.ejs', {
+        message: req.query.message ? req.query.message : null
+    })
+})
+
+// POST /users/login - ingest data from form rendered @ GET /users/login
+router.post('/login', async (req, res) => {
+    try {
+        // look up the user based on their email
+        const user = await db.user.findOne({
+            where: {
+                email: req.body.name
+            }
+        })
+        // boiler plate message if login fails
+        const badCredentMessage = 'username or password incorrect'
+        if(!user) {
+            // if user isnt found in db
+            res.redirect('/users/login?message=' + badCredentMessage)
+        } else if (user.password !== req.body.password) {
+            // if the users supplied password is incorrect
+            res.redirect('/users/login?message=' + badCredentMessage)
+        } else {
+            // if the user is found and their password matches log them in
+            console.log('loggin user in')
+            res.cookie('userId', user.id)
+            res.redirect('/')
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Server Error')
+    }
+})
+
+// GET /users/logout -- clear any cookies and redirect to the homepage
+router.get('/logout', (req, res) => {
+    res.send('log the user out by clearing the cookie')
+})
+
 // export the router
 module.exports = router
